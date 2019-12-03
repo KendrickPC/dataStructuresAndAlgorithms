1954583fc90b5c40cdc9aeb9f967abf8efc53f72
kendrickpc <kennethpchang@gmail.com>
2019-12-03 18:55:42 +0800

    Ended on page 100.

---
+++ Chapter2/2.4.abstract_base_classes.py
@@ -0,0 +1,31 @@
+# An abstract base class akin to collections.Sequence
+
+from abc import ABCMeta, abstractmethod
+
+class Sequence(metaclass=ABCMeta):
+    @abstractmethod
+    def __len__(self):
+        pass
+
+    @abstractmethod
+    def __getitem__(self, j):
+        pass
+
+    def __contains__(self, val):
+        for j in range(len(self)):
+            if self[j] == val:
+                return True
+            return False
+
+    def index(self, val):
+        for j in range(len(self)):
+            if self[j] == val:
+                return j
+            raise ValueError('value not in sequence')
+
+    def count(self, val):
+        k = 0
+        for j in range(len(self)):
+            if self[j] == val:
+                k += 1
+        return k
--- Chapter2/2.4.progressions.py
+++ Chapter2/2.4.progressions.py
@@ -20,3 +20,56 @@ class Progression:
         print(' '.join(str(next(self)) for j in range(n)))
 
     
+# A class that produces an arithmetic progression.
+class ArithmeticProgression(Progression):
+
+    def __init__(self, increment=1, start=0):
+        super().__init__(start)
+        self._increment = increment
+
+    def _advance(self):
+        self._current += self._increment
+
+
+# A class that produces a geometric progression.
+class GeometricProgression(Progression):
+
+    def __init__(self, base=2, start=1):
+        super().__init__(start)
+        self._base = base
+
+    def _advance(self):
+        self._current *= self._base
+
+# A class that produces a Fibonacci progression
+class FibonacciProgression(Progression):
+    def __init__(self, first=0, second=1):
+        super().__init__(first)
+        self._prev = second - first
+
+
+    def _advanced(self):
+        self._prev, self._current = self._current, self._prev + self._current
+
+
+# Output of the unit tests from Code Fragment
+if __name__ == '__main__':
+    print('\nDefault progression:')
+    Progression().print_progression(10)
+
+    print('\nArithmetic Progression')
+    ArithmeticProgression(5).print_progression(10)
+
+    print('\nGeometric Progression with default base:')
+    GeometricProgression().print_progression(10)
+
+    print('\nGeometric Progression with default base:')
+    GeometricProgression(3).print_progression(10)
+    
+    print('\nGeometric Progression with default base:')
+    FibonacciProgression().print_progression(10)
+
+    print('\nGeometric Progression with default base:')
+    FibonacciProgression(4, 6).print_progression(10)
+
+
